b7cd7bf9523f72e5a8c051bef30e5bde163deae9
Jin2oo2 <nojinhiroaki@icloud.com>
2024-09-30 13:51:34 +0100

    api functions

--- frontend/src/api/analyseFeedback.js
+++ frontend/src/api/analyseFeedback.js
@@ -1,21 +1,36 @@
 import axios from 'axios'
 import dotenv from 'dotenv'
 
+
+// setting up API url from .env file
 dotenv.config();
 const apiUrl = process.env.VITE_API_URL;
 
-export const analyseFeedback = async (feedbackText) => {
+
+// POST method for feedback analysis
+export const analyseFeedback = async (feedbackText, language_code) => {
   try {
     const response = await axios.post(apiUrl, {
       "feedback": feedbackText,
-      "language_code": "en"
+      "language_code": language_code
     })
   
     console.log('Response data:', response.data)
+    return response.data
   } catch (error) {
     console.error('Error posting feedback text: ', error)
   }
 }
 
-// analyseFeedback("It's a crazy website in a good way.")
-// console.log('Hello!!')
+// GET method for feedback analysis
+export const getFeedbackResults = async () => {
+  try {
+    const response = await axios.get(apiUrl)
+    console.log('Response data:', response.data)
+
+    return response.data
+
+  } catch (error) {
+    console.error('Error getting feedback analysis results from database: ', error)
+  }
+}
